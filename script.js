bf801d04eceaa51b5975867821c58807555ddcfb
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-09-10 12:16:48 +0100

    Add screen controller to build DOM

--- script.js
+++ script.js
@@ -238,11 +238,68 @@ function GameController(
         playRound,
         getActivePlayer,
         simulateGamePlayer1Win,
+        getBoard: board.getBoard
         // simulateGameTie
     };
 }
 
-const game = GameController();
+// const game = GameController();
+
 
     
 
+function ScreenController() {
+
+    const game = GameController();
+    const playerTurnDiv = document.querySelector('.turn');
+    const boardDiv = document.querySelector('.board');
+
+    const updateScreen = () => {
+        // clear the board
+        boardDiv.textContent = "";
+
+        // get the newest version of the board and player turn
+        const board = game.getBoard();
+        const activePlayer = game.getActivePlayer();
+
+        // Display player's turn
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+        // Render board squares
+        board.forEach((row,rowIndex) => {
+            row.forEach((cell, colIndex) => {
+                // Anything clickable should be a button!!
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                // Create a data attribute to identify the column
+                // This makes it easier to pass into our `playRound` function 
+                cellButton.dataset.column = colIndex
+                cellButton.dataset.row = rowIndex
+                console.log(cellButton.dataset.column)
+                console.log(cellButton.dataset.row)
+                console.log("ok")
+                cellButton.textContent = cell.getValue();
+                boardDiv.appendChild(cellButton);
+            })
+        })
+    }
+
+    // Add event listener for the board
+    function clickHandlerBoard(e) {
+        const selectedColumn = e.target.dataset.column;
+        const selectedRow = e.target.dataset.row;
+        // Make sure I've clicked a column and not the gaps in between
+        if (!selectedColumn || !selectedRow) return;
+
+        game.playRound(selectedRow,selectedColumn);
+        updateScreen();
+    }
+    boardDiv.addEventListener("click", clickHandlerBoard);
+
+    // Initial render
+    updateScreen();
+
+    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+}
+
+ScreenController();
